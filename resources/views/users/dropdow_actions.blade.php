60213f2a424fb28fc6bcb2513a42b43168e1ce47
fqueta <ger.maisaqui1@gmail.com>
2022-06-28 14:01:15 -0300

    4

--- resources/views/users/dropdow_actions.blade.php
+++ resources/views/users/dropdow_actions.blade.php
@@ -4,8 +4,8 @@
         Exportar
     </button>
     <div class="dropdown-menu" id="dp-exportar" aria-labelledby="triggerId">
-        <a class="dropdown-item" href="{{ route('familias.export_filter') }}" exportar-filter target="_BLANK"> <i class="fas fa-file-excel"></i> Excel (Página)</a>
-        <a class="dropdown-item" href="{{ route('familias.export_all') }}" target="_BLANK"> <i class="fas fa-file-excel"></i> Excel (Tudo)</a>
+        {{-- <a class="dropdown-item" href="{{ route('familias.export_filter') }}" exportar-filter target="_BLANK"> <i class="fas fa-file-excel"></i> Excel (Página)</a>
+        <a class="dropdown-item" href="{{ route('familias.export_all') }}" target="_BLANK"> <i class="fas fa-file-excel"></i> Excel (Tudo)</a> --}}
         <button class="dropdown-item" onclick="window.print();"> <i class="fas fa-print"></i> Impressora</button>
     </div>
 </div>
