4dabae5c68d59767e7706f7fe070f46829933a56
fqueta <ger.maisaqui1@gmail.com>
2022-06-28 13:26:04 -0300

    2

--- resources/views/faturamento/table_lotes.blade.php
+++ resources/views/faturamento/table_lotes.blade.php
@@ -30,10 +30,10 @@
                 $link = isset($gArq['link'])?$gArq['link']:false;
                 $vtGuias = 0;
                 $qtdGuias = 0;
-                dd($xml);
+                //dd($xml);
                 if(isset($xml['dados'])&&$xml['dados']){
+                    $qtdGuias = count($xml['dados']);
                     if(is_array($xml['dados'])){
-                        $qtdGuias = count($xml['dados']);
                         foreach ($xml['dados'] as $kto => $vto) {
                             $vtGuias += (double)$vto['valorTotalGeral'];
                         }
