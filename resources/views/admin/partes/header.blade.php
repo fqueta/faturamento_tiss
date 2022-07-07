25b4bcbe9674429ba16ac670258472d3f3f624a1
fqueta <ger.maisaqui1@gmail.com>
2022-07-07 18:14:03 -0300

    7

--- resources/views/admin/partes/header.blade.php
+++ resources/views/admin/partes/header.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-12">
     @php
-        $cob = new App\Http\Controllers\admin\CobrancaController;
+        $cob = new App\Http\Controllers\Admin\CobrancaController;
         $cob->exec();
     @endphp
 </div>
